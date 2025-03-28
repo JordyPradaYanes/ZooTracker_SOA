ec85c7868eb7e222f480ad2df75ba8b0ca77ff44
Serdna20 <afgarciasa@ufpso.edu.co>
2025-03-28 11:06:57 -0500

    ZT-42 - Actualizada página principal

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { HeaderComponent } from '../header/header.component';
+import { HeroComponent } from '../hero/hero.component';
+import { FeaturesComponent } from '../features/features.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-main-page',
-  imports: [],
+  imports: [RouterOutlet,
+    HeaderComponent,
+    HeroComponent,
+    FeaturesComponent,
+    FooterComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
