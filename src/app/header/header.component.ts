42cc772dea13f861bc68064d00baa63e5652fbd8
Serdna20 <afgarciasa@ufpso.edu.co>
2025-03-29 00:44:04 -0500

    ZT-47 - Crear servicio firebase + Fix de reportes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,7 +4,7 @@ import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
