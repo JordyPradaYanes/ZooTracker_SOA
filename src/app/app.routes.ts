6e4aa8dda75a1001960e7556ef4fa2f0f88fccca
Serdna20 <afgarciasa@ufpso.edu.co>
2025-03-28 21:42:29 -0500

    ZT-33 - Añadido Componente Login y HTML

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 
 import { MainPageComponent } from './main-page/main-page.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-	{path: '', component: MainPageComponent},
-	{path: 'regresar', component: MainPageComponent},
+	{ path: '', component: MainPageComponent},
+	{ path: 'regresar', component: MainPageComponent},
+	{ path: 'login', component: LoginComponent},
 	{ path: '**', redirectTo: '' }
 ];
 
